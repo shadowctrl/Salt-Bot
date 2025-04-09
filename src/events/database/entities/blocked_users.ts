2445b41158d69d3498dc7831227c5d094848ffde
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2025-04-10 00:01:34 +0530

    feat: implement user blocking functionality with PostgreSQL integration

---
+++ src/events/database/entities/blocked_users.ts
@@ -0,0 +1,37 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, ManyToOne } from "typeorm";
+import { IBlockUser, IBlockReason } from "../../../types";
+
+@Entity("blocked_users")
+export class BlockedUser implements IBlockUser {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @Column({ nullable: false })
+    userId!: string;
+
+    @Column({ default: true })
+    status!: boolean;
+
+    @OneToMany(() => BlockReason, blockReason => blockReason.blockedUser, {
+        cascade: true,
+        eager: true
+    })
+    data!: BlockReason[];
+}
+
+@Entity("block_reasons")
+export class BlockReason implements IBlockReason {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @Column({ nullable: false })
+    reason!: string;
+
+    @CreateDateColumn()
+    timestamp!: Date;
+
+    @ManyToOne(() => BlockedUser, blockedUser => blockedUser.data, {
+        onDelete: "CASCADE"
+    })
+    blockedUser!: BlockedUser;
+}
